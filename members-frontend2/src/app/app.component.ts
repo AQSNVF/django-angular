29257357bd9998e4e8e9e01b3d228e3565feb94f
AQSNVF <nelson.freire@ativa-qs.com.br>
2020-01-14 15:41:27 -0300

    Renomeando temporariamente a Pasta Members-Frontend

---
+++ members-frontend2/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { ApiService } from './api.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'members-frontend';
+
+  members = [
+    {name: 'Member 01', id: 1, surname: 'Ciclano', photo: 'http://www.minhaapp.com/photo1'},
+    {name: 'Member 02', id: 2, surname: 'Beltrano', photo: 'http://www.minhaapp.com/photo2' },
+    {name: 'Member 03', id: 3, surname: 'Fulano', photo: 'http://www.minhaapp.com/photo3' },
+    {name: 'Member 04', id: 4, surname: 'Coisano', photo: 'http://www.minhaapp.com/photo4' },
+  ];
+
+  constructor(private api: ApiService) {
+    this.getMembers();
+  }
+
+  getMembers = () => {
+    this.api.getAllMembers().subscribe(
+      data => {
+        this.members = data;
+      },
+      error => {
+        console.log('Aconteceu um Erro!', error.message);
+      }
+    );
+  }
+
+  memberClicked = (member) => {
+    this.api.getMember(member.id).subscribe(
+      data => {
+        console.log(data);
+      },
+      error => {
+        console.log('Aconteceu um Erro!', error.message);
+      }
+    );
+  }
+}
